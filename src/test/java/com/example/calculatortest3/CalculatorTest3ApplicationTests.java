54dc1f3e78caf35947d51c8285045f8424afe3b0
17152 <nazarovtolya2@gmail.com>
2023-06-19 19:01:34 +0500

    началло

---
+++ src/test/java/com/example/calculatortest3/CalculatorTest3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculatortest3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorTest3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
